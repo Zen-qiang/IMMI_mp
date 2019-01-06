8e7b5905cb5be98b2220d0e1484ea47f5ce66fb8
molong <mo.long@dlt-world.com>
2019-01-06 10:22:25 +0800

    重新提交
    冲突

--- pages/account/index.js
+++ pages/account/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // pages/account/index.js 我的界面
 const app = getApp();
 import config from '../../config.js';
@@ -94,4 +95,97 @@ Page({
     });
     
   }
+=======
+// pages/account/index.js 我的界面
+const app = getApp();
+import config from '../../config.js';
+var WxParse = require('../../common/lib/wxParse/wxParse.js');
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userName: "",
+    accountInfo: "",
+    userbg:"",
+  },
+
+  checkLogin() {
+    wx.navigateTo({
+      url: '/pages/login/index',
+    });
+  },
+
+  onLoad: function() {
+    this.getLayout();
+  },
+
+  prepareData() {
+    let that = this;
+    var data = {
+      url: config.getAccountInfo,
+      params: {}
+    }
+    app.nGet(data).then(data => {
+      this.setData({
+        accountInfo: data.data
+      });
+      let webContent = data.data ? data.data : '';
+      WxParse.wxParse('webContent', 'html', webContent, that, 0);
+    }, res => {});
+  },
+
+  getLayout() {
+    var data = {
+      url: config.getLayout,
+      params: {}
+    }
+    app.nGet(data).then(data => {
+      console.log(data);
+      this.setData({
+        userbg: data.data.userbg
+      });
+    }, res => { });
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+    let userName = app.getValue('username');
+    if (userName && userName.length > 0) {
+      this.setData({
+        userName: userName
+      });
+      this.prepareData();
+    }
+  },
+  /**
+   * 进入我的订单、退货单、补货单、地址
+   * 判断是否登录 未登录先去登录页面
+   * 
+   */
+  enterOtherPage(e) {
+    let username = this.data.userName;
+    if (!username) {
+      wx.navigateTo({
+        url: '/pages/login/index',
+      });
+    } else {
+      let page = e.currentTarget.dataset.page;
+      if (page === 'contactsManager') {
+        wx.navigateTo({
+          url: `/pages/${page}/${page}`,
+        })
+      } else {
+        // console.log(page);
+        wx.navigateTo({
+          url: `/pages/${page}/index`,
+        })
+      }
+    }
+  }
+>>>>>>> master
 })
--- pages/pdtList/index.js
+++ pages/pdtList/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // pages/pdtList/index.js
 const app = getApp();
 import config from '../../config.js';
@@ -113,4 +114,213 @@ Page({
       });
     }
   }
+=======
+// pages/pdtList/index.js
+const app = getApp();
+import config from '../../config.js';
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    type: "",
+    search: '',
+    attrIdList: [],
+    page: 1,
+    size: 10,
+    pages: 0,
+    total: 0,
+    list: [],
+    loadMore: { // 加载信息
+      title: "",
+      isLoad: false
+    },
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    if (options.type) {
+      this.setData({
+        type: options.type
+      });
+    } else {
+      if (options.search) {
+        this.setData({
+          search: options.search,
+          type: '3'
+        });
+      }
+      if (options.attrIdList) {
+        let attrIdList = options.attrIdList.split(',');
+        this.setData({
+          attrIdList: attrIdList,
+          type: '3'
+        });
+      }
+    }
+    this.prepareData();
+  },
+
+  prepareData(loadmore) {
+    if (this.data.type == '1') {
+      this.getType1Data(loadmore);
+    } else if (this.data.type == '2') {
+      this.getType2Data(loadmore);
+    } else {
+      this.getPdtList(loadmore);
+    }
+  },
+
+  // 新品推荐
+  getType1Data(loadmore) {
+    var data = {
+      url: config.indexNewPdtQuery,
+      params: {
+        recommend: true,
+        page: this.data.page,
+        size: this.data.size
+      }
+    }
+    app.nGet(data).then(data => {
+      if (data.data && data.data.list) {
+        if (!loadmore) {
+          if (data.data.list && data.data.list.length === 0) {
+            this.setData({
+              ["loadMore.title"]: '暂无数据'
+            });
+          }
+          this.setData({
+            page: data.data.page,
+            size: data.data.size,
+            pages: data.data.pages,
+            total: data.data.total,
+            list: data.data.list,
+          });
+        } else {
+          let list = [...this.data.list, ...data.data.list];
+          this.setData({
+            ["loadMore.isLoad"]: false,
+            page: data.data.page,
+            size: data.data.size,
+            pages: data.data.pages,
+            total: data.data.total,
+            list: list,
+          });
+        }
+      }
+    }, res => {
+      this.setData({
+        ["loadMore.isLoad"]: false
+      });
+    });
+  },
+
+  // Top 20
+  getType2Data(loadmore) {
+    let attrIdList = JSON.stringify(this.data.attrIdList);
+    var data = {
+      url: config.searchB2bProductTopList,
+      params: {
+        page: 1,
+        size: 20,
+        orderBy: 'QTY',
+        sort: "DESC",
+      }
+    }
+    app.nGet(data).then(data => {
+      if (data.data && data.data.list) {
+        if (data.data.list && data.data.list.length === 0) {
+          this.setData({
+            ["loadMore.title"]: '暂无数据'
+          });
+        } else {
+          this.setData({
+            page: 1,
+            size: 20,
+            pages: 1,
+            total: 20,
+            list: data.data.list,
+            ["loadMore.isLoad"]: false,
+          });
+        }
+      }
+    }, res => {
+      this.setData({
+        ["loadMore.isLoad"]: false
+      });
+    });
+  },
+
+  getPdtList(loadmore) {
+    let attrIdList = JSON.stringify(this.data.attrIdList);
+    var data = {
+      url: config.indexNewPdtQuery,
+      params: {
+        search: this.data.search,
+        attrIdList: attrIdList,
+        page: this.data.page,
+        size: this.data.size
+      }
+    }
+    app.nGet(data).then(data => {
+      if (data.data && data.data.list) {
+        if (!loadmore) {
+          if (data.data.list && data.data.list.length === 0) {
+            this.setData({
+              ["loadMore.title"]: '暂无数据'
+            });
+          }
+          this.setData({
+            page: data.data.page,
+            size: data.data.size,
+            pages: data.data.pages,
+            total: data.data.total,
+            list: data.data.list,
+          });
+        } else {
+          let list = [...this.data.list, ...data.data.list];
+          this.setData({
+            ["loadMore.isLoad"]: false,
+            page: data.data.page,
+            size: data.data.size,
+            pages: data.data.pages,
+            total: data.data.total,
+            list: list,
+          });
+        }
+      }
+    }, res => {
+      this.setData({
+        ["loadMore.isLoad"]: false
+      });
+    });
+  },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+    wx.showNavigationBarLoading();
+    wx.hideNavigationBarLoading();
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+    if (Number(this.data.page) < Number(this.data.pages)) {
+      this.setData({
+        ["loadMore.isLoad"]: true,
+        page: Number(this.data.page) + 1,
+      });
+      this.prepareData('loadmore');
+    } else {
+      this.setData({
+        ["loadMore.title"]: '没有更多数据啦'
+      });
+    }
+  }
+>>>>>>> master
 })
